3bffb442f947791a40059a1c0a8700e80889b10b
Jan Richter <jri@JRi.local>
2020-01-17 14:55:50 +0100

    final

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Aalen University!');
-}).listen(8080);
+}).listen(5005);
