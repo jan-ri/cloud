7d5a8aead0f7baef3131ffd06408ecbf3d4dbc7c
Jan Richter <jri@JRi.local>
2020-01-17 14:54:25 +0100

    keep it simple

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>Hello World!</h1>');
-  res.end();
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Aalen University!');
 }).listen(8080);
